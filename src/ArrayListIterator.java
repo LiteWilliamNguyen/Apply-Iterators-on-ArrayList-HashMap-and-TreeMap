ba76f46ae2af2af89e0679a652382d8aa7141956
William Nguyen(Khanh Trinh) <49286034+LiteWilliamNguyen@users.noreply.github.com>
2024-11-23 20:35:54 -0800

    2nd commit

--- src/ArrayListIterator.java
+++ src/ArrayListIterator.java
@@ -7,40 +7,22 @@ public class ArrayListIterator {
         // Step 1: Create and populate an ArrayList with random integers
         ArrayList<Integer> numbers = new ArrayList<>();
         Random random = new Random();
-        for (int i = 0; i < 10; i++) {
-            numbers.add(random.nextInt(100)); // Random integers between 0 and 99
+        for (int i = 0; i < 10; i++){
+            numbers.add(random.nextInt(100)) // Random integer between 0 and 99
         }
-        System.out.println("Original List: " + numbers);
+        System.out.println("List: " + numbers);
 
         // Step 2: Iteration using an Iterator and calculating sum
         Iterator<Integer> iterator = numbers.iterator();
         int sum = 0;
-        while (iterator.hasNext()) {
+        while(iterator.hasNext()){
             int num = iterator.next();
             System.out.println("Number: " + num);
             sum += num;
         }
-        System.out.println("Total Sum: " + sum);
+        System.out.println("Total sum: "+ sum);
 
-        // Step 3: Modification during iteration
-        try {
-            iterator = numbers.iterator();
-            while (iterator.hasNext()) {
-                int num = iterator.next();
-                if (num % 2 == 0) {
-                    iterator.remove(); // Using iterator's remove method
-                }
-            }
-            System.out.println("List after removing even numbers using iterator: " + numbers);
+        //
 
-            // Adding an element to the list during iteration
-            for (int i = 0; i < numbers.size(); i++) {
-                if (numbers.get(i) % 2 != 0) {
-                    numbers.add(77); // This will throw a ConcurrentModificationException
-                }
-            }
-        } catch (Exception e) {
-            System.out.println("Exception during modification: " + e);
-        }
     }
 }
